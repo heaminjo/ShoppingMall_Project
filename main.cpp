656267628247798deff79bb6851f47cbdd1788b9
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-02-07 02:07:27 +0900

    main03.cpp
    리스트와 스택 두가지를 다루는데 복잡하므로
    전부 리스트로 통일함

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@
 //상품 추가를 2번 할수없는 오류
 int main(void) {
 	int select;
-	List* CategoryList = new List();  //카테고리 리스트
-	Stack* MainStack = new Stack();
+	CategoryList* categoryList = create_CategoryList();  //카테고리 리스트
+	List* MainList =create_List();
 	while (1) {
 		MainManu();
 		select = choice();
 		switch (select)
 		{
 		case 1: { //고객 페이지
-			print_Stack(MainStack);
+			print_List(MainList);
 			Customer_Page();
 			break;
 		}
@@ -23,31 +23,32 @@ int main(void) {
 			switch (select)
 			{
 			case 1: { //카테고리 추가
-				Category_Push(CategoryList);
+				Category_Push(categoryList);
 				break;
 			}
 			case 2: {  //상품 추가
 				//카테고리가 없는 경우
-				if (CategoryList->count == 0) {
+				if (categoryList->count == 0) {
 					printf("카테고리가 없습니다.\n");
 					break;
 				}
-				print_Category(CategoryList);
+				print_Category(categoryList);
 				select = choice();
 
 				//만약 카테고리 번호 중 없는 번호를 고를경우
-				if (select > CategoryList->count) {
+				if (select > categoryList->count) {
 					printf("잘못된 번호입니다.");
 					break;
 				}
 				Product* product = create_product();
-				List_Push(CategoryList, product,select); //카테고리 상품 추가
-				Stack_Push(MainStack, product); //메인 상품 추가
+				List* list = Category_PushList(categoryList,select); //카테고리 상품 추가
+				printf("리스트:%s 추가\n", list->CategoryName);
+				Product_Push(MainList, product); //메인 상품 추가
+				Product_Push(list, product);
 				break;
 			}
 			case 3: {  //상품 제거
-				print_Stack(MainStack);
-				select = choice();
+				
 			}
 			case 5: {
 				break;
