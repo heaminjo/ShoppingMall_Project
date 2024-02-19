15819acda5dd95cbbd85a59e5507169e16b9a7c4
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-02-19 23:58:16 +0900

    Update main.cpp
    로그인 기능 추가

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ bool on = false;
 List* create_List(); //리스트 생성 및 초기화
 CategoryList* create_CategoryList(); //카테고리 리스트 생성 및 초기화
 List* Category_Find(CategoryList* CategoryList, int count); //카테고리 탐색
-void Product_Push(List* list, Product*product,int *number); //상품 추가
+void Product_Push(List* list, Product*product,int* number); //상품 추가
 void Remove_Product(List* list, int number); //상품 제거
 void print_CategoryNum(CategoryList* list); //카테고리 선택 창
 void print_List(List* list); //리스트 출력
@@ -19,14 +19,26 @@ bool Consent();     //회원 가입 동의 여부
 Member_List* create_MemberList(); // 회원 목록 생성 및 초기화
 bool OverLap_Check(Member_List* list, char id[]); //아이디 중복 체크
 bool Password_Check(char pw[], char pw2[]); //비밀번호 재확인 체크
-Member* create_Member(Member_List* list);//회원 정보 입력 후 생성
+Member* create_Member(); //회원 생성 및 초기화
+Member* create_Member_Data(Member_List* list,Member*member);//회원 정보 새로입력
 void Member_Push(Member_List* list, Member* member);//회원 저장
+void Member_Print(Member_List* list);  //회원 목록 출력
+Member_Page* create_Page();   //회원페이지 생성 및 초기화
+char* ID_Enter();  //아이디 입력
+char* Password_Enter();  //비밀번호 입력
+
+//아이디&비밀번호 일치 체크
+bool Login_Member_Search(Member_Page* page, Member_List* list, char* id, char* pw);
+void Page_Login(Member_Page* page, Member* member);//페이지에 로그인
+void Page_print(Member_Page* page); //회원 페이지 출력
 
 int main(void) {
+
 	int select;
-	CategoryList* categoryList = create_CategoryList();  //카테고리 리스트
-	List* Represent = create_List();  //대표 카테고리
-	Member_List* MemberList = create_MemberList();  //회원 리스트
+	CategoryList* CategoryList = create_CategoryList();  //카테고리 리스트
+	List* Represent = create_List();
+	Member_List* MemberList = create_MemberList();
+	Member_Page* memberpage = create_Page();
 	while (1) {
 		MainManu();
 		select = choice();
@@ -35,7 +47,8 @@ int main(void) {
 		case 1: { //고객 페이지
 			print_List(Represent);
 			Customer_Page();
-			select = choice(); 
+			select = choice();
+ 
 			switch (select)
 			{
 			case 1: {  //카테고리
@@ -44,17 +57,33 @@ int main(void) {
 			case 2: {  //찜
 			}
 			case 3: {  //마이페이지
-				if (on == true) {           //로그인 on
+				if (on == true) {
+					Page_print(memberpage);
 					JoinMenu();
 					select = choice();
 				}
-				else if(on == false) {      //로그인 off
+				else if(on == false) {
 					NotJoinMenu();
 					select = choice();
 					switch (select)
 					{
 					case 1: { //로그인
+						bool check;
+						while (1) {
+							//아이디&비밀번호 입력
+							char * id = ID_Enter();
+							char* pw = Password_Enter();
 
+							check = Login_Member_Search(memberpage, MemberList, id,pw);
+							if (check == true) {
+								on = true;
+								break;
+							}
+							else {
+								printf("아이디 또는 비밀번호가 틀렸습니다.\n");
+							}
+						}
+						break;
 					}
 					case 2: { //회원가입
 						bool check;
@@ -66,7 +95,8 @@ int main(void) {
 							break;
 						}
 						else if (check == true) {
-							Member* member = create_Member(MemberList);
+							Member* member = create_Member();
+							create_Member_Data(MemberList,member);
 							Member_Push(MemberList, member);
 							printf("회원가입이 성공적으로 끝났습니다.\n");
 							printf("\n");
@@ -77,7 +107,11 @@ int main(void) {
 						break;
 					}
 				}
-				}
+				break;
+			}
+			default:
+				break;
+			}
 			break;
 		}
 		case 2: {  //관리자 페이지
@@ -86,31 +120,32 @@ int main(void) {
 			switch (select)
 			{
 			case 1: { //카테고리 추가
-				Category_Push(categoryList);
+				Category_Push(CategoryList);
 				break;
 			}
 			case 2: {  //대표 카테고리 설정
-			print_CategoryNum(CategoryList);
-			select = choice();
+				print_CategoryNum(CategoryList);
+				select = choice();  //카테고리 선택
 
-			Represent= Category_Find(CategoryList, select);				printf("대표 카테고리가 설정 되었습니다.\n");
-			break;
+				Represent= Category_Find(CategoryList, select);
+				printf("대표 카테고리가 설정 되었습니다.\n");
+				break;
 			}
 			case 3: {  //상품 추가
 				//카테고리가 없는 경우
-				if (categoryList->count == 0) {
+				if (CategoryList->count == 0) {
 					printf("카테고리가 없습니다.\n");
 					break;
 				}
-				print_Category(categoryList);
+				print_CategoryNum(CategoryList);
 				select = choice();
-
+				
 				//만약 카테고리 번호 중 없는 번호를 고를경우
-				if (select > categoryList->count) {
+				if (select > CategoryList->count) {
 					printf("잘못된 번호입니다.");
 					break;
 				}
-
+			
 				List* categoryList = Category_Find(CategoryList,select); //추가할 카테고리 가져오기
 				Product* product = create_product();
 				Product_Push(categoryList,product,&number);
@@ -118,12 +153,11 @@ int main(void) {
 				break;
 			}
 			case 4: {  //상품 제거
-				
 				if (CategoryList->count == 0) {
 					printf("카테고리가 없습니다.\n");
 					break;
 				}
-				print_Category(CategoryList); 
+				print_CategoryNum(CategoryList);
 				select = choice();  //카테고리 선택
 				List* categorylist = Category_Find(CategoryList, select); //추가할 카테고리 가져오기
 				if (categorylist->count == 0) {
@@ -132,11 +166,14 @@ int main(void) {
 				}
 				print_List(categorylist);
 				select = choice();  //상품 선택
-				
+
 				Remove_Product(categorylist, select);
 				break;
 			}
-			case 5: {
+			case 5: {  //구매하기
+
+			}
+			case 6: {
 				break;
 			}
 			default:
